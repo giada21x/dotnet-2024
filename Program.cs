50e60c21d374e2d1332b19ce07bcd70f7a43d38d
unknown <giada01.adamo@gmail.com>
2024-01-09 15:49:42 +0100

    comparazione tra interi

--- Program.cs
+++ Program.cs
@@ -5,9 +5,14 @@
             int a = 10;
             int b = 20;
             bool maggiore = a > b;
-            Console.WriteLine($"Il primo numero è maggiore del secondo? {maggiore}"); 
+            bool minore = a < b;
+            bool uguale = a == b;
+            Console.WriteLine($"Il primo numero è maggiore del secndoo? {maggiore}"); 
+            Console.WriteLine($"Il primo numero è maggiore del secndoo? {minore}"); 
+            Console.WriteLine($"Il primo numero è maggiore del secndoo? {uguale}");
        }
-    }       
+    }     
+         
     
     
     
