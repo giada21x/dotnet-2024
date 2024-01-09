01ca8a175cc07f62d8fb5a13159aac533eafe696
unknown <giada01.adamo@gmail.com>
2024-01-09 12:18:11 +0100

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "Giada";
+            string cognome = "Adamo";
+            Console.WriteLine($"Ciao {nome} {cognome}");
+            Console.WriteLine("Ciao " + nome + " " + cognome);
+    
+        }
+     }
+    
