3a03b94bdaa72accead072d46d9525c8b4c04945
unknown <giada01.adamo@gmail.com>
2024-01-11 10:56:45 +0100

    lista con il metodo Count

--- Program.cs
+++ Program.cs
@@ -1,14 +1,15 @@
-﻿ class Program 
+﻿class Program 
     {
         static void Main(string[] args)
          {
             List<string> nomi = new List<string>(); 
             nomi.Add("Mario"); 
             nomi.Add("Luigi"); 
-            nomi.Add("Giovanni");
+            nomi.Add("Giovanni"); 
             Console.WriteLine($"Ciao {nomi[0]}, {nomi[1]}, {nomi[2]}");
+            Console.WriteLine($"Il numero di elementi è {nomi.Count}");
          }
-    }  
+    } 
       
   
     
