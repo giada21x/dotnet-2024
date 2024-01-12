dd999e7f2c89b848ebbee5357436339a4eb0b168
unknown <giada01.adamo@gmail.com>
2024-01-12 09:46:28 +0100

    if-else if-else

--- Program.cs
+++ Program.cs
@@ -4,13 +4,21 @@
     {
         int a = 10;
         int b = 20;
-        if (a > b) //la condizione da  verificare si scrive tra parentesi
+        if (a > b) //la condizione da verificare si scrive tra parentesi
         {
             Console.WriteLine($"{a} è maggiore di {b}");
+        } 
+        else  
+        {
+            Console.WriteLine($"{a} è uguale a {b}");
         }
+        else if (a < b)
+        {
+            Console.WriteLine($"{a} è minore di {b}");
+        }    
+        
     }
 }
-        
     
     
     
