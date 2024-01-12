7f6e0397aa0ae0a36099c1076afbba61db1ff7e2
unknown <giada01.adamo@gmail.com>
2024-01-12 12:54:17 +0100

    ReadLine e ReadKey

--- Program.cs
+++ Program.cs
@@ -1,25 +1,8 @@
-﻿//array di stringhe con tre nomi. cicliamo con foreach e se il nome è uguale a mario con metodo add aggiunge il nome ad una lista
-class Program 
+﻿class Program
 {
     static void Main(string[] args)
     {
-        string[] nomi = ["Mario", "Luigi", "Giovanni"];
-        
-        //creare una lista di destinazione
-        List<string> lista = new List<string>(); 
-        //creare un ciclo che iteri sugli elementi dell'array
-        foreach (string nome in nomi) 
-        {
-        //se il nome è uguale a 'Mario' con metodo add aggiunge il nome ad una lista
-            if (nome == "Mario") 
-            {
-                lista.Add(nome);
-            }
-        } 
-        //versioniamo e veifichiamo che 'Mario' sia stato aggiunto alla lista
-        foreach (string nome in lista)
-        {
-            Console.WriteLine($"{nome}");
-        }
-     }
+        Console.WriteLine("Premi un tasto per terminare...");
+        Console.ReadKey();
+    }
 }
