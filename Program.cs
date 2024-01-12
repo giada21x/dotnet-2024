2c0a74b39eeb8542aa418a33b1b759de00a510c6
unknown <giada01.adamo@gmail.com>
2024-01-12 10:32:29 +0100

    dizionario di stringhe e numeri

--- Program.cs
+++ Program.cs
@@ -2,12 +2,16 @@
 {
     static void Main(string[] args)
     {
-        Dictionary<string, string> nomi = new Dictionary<string, string>();
-        nomi.Add("Mario", "Rossi");
-        nomi.Add("Luigi", "Verdi");
-        nomi.Add("Giovanni", "Bianchi");
-        Console.WriteLine($"Ciao {nomi["Mario"]} {nomi["Luigi"]} e {nomi["Giovanni"]}");
+        Dictionary<string, int> eta = new Dictionary<string, int>
+        {
+            {"Mario", 25},
+            {"Luigi", 30},
+            {"Giovanni", 35}
+        };
+            Console.WriteLine($"L'età dei tre ragazzi è {eta["Mario"]} {eta["Luigi"]} e {eta["Giovanni"]}");
+        
     }
 }
+
     
     
