9678b5baf92ae672a87c69d33fe51915b0bcd174
unknown <giada01.adamo@gmail.com>
2024-01-12 10:16:36 +0100

    dizionario di stringhe

--- Program.cs
+++ Program.cs
@@ -1,21 +1,12 @@
-﻿ class Program 
+﻿class Program 
 {
     static void Main(string[] args)
     {
-        int a = 10;
-        int b = 20;
-        switch (a)
-        {
-            case 10:
-                Console.WriteLine($"{a} è uguale a 10");
-                break;
-            case 20:
-                Console.WriteLine($"{a} è uguale a 20"); 
-                break;
-            default:
-                Console.WriteLine($"{a} non è uguale a 10 o 20");
-                break;
-        }
+        Dictionary<string, string> nomi = new Dictionary<string, string>();
+        nomi.Add("Mario", "Rossi");
+        nomi.Add("Luigi", "Verdi");
+        nomi.Add("Giovanni", "Bianchi");
+        Console.WriteLine($"Ciao {nomi["Mario"]} {nomi["Luigi"]} e {nomi["Giovanni"]}");
     }
 }
     
