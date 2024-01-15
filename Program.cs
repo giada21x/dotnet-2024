d99f22856cc05f27b7b832a3e62bcd318b7abdf6
unknown <giada01.adamo@gmail.com>
2024-01-15 09:13:39 +0100

    ciclo while

--- Program.cs
+++ Program.cs
@@ -2,7 +2,17 @@
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Premi un tasto per terminare...");
-        Console.ReadKey();
+        string[] nomi = new string[3];
+        nomi[0] = "Mario";
+        nomi[1] = "Luigi";
+        nomi[2] = "Giovanni";
+        // string[] nomi = new string[] { "Mario", "Luigi", "Giovanni" };
+
+        int i = 0; // Variabile indice inizializzata a 0
+        while (i < nomi.Length) // Il ciclo continua finché l'indice è minore della lunghezza dell'array
+        {
+            Console.WriteLine($"Ciao {nomi[i]}");
+            i++; // Incremento dell'indice
+        }
     }
 }
