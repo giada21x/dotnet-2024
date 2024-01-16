2e82abd7f177392c9e63eb9703d90addec5fc78b
unknown <giada01.adamo@gmail.com>
2024-01-16 09:28:27 +0100

    esercizio inuput complessi

--- Program.cs
+++ Program.cs
@@ -2,23 +2,50 @@
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Trascina un file qui e premi invio:");
-        string filePath = Console.ReadLine().Trim('"'); //Rimuove le virgolette che possono apparire nel percorso
+        Console.Clear();
+        System.Console.WriteLine("1. Inserisci opzione uno ");
+        System.Console.WriteLine("2. Insersci opzione due ");
+        System.Console.WriteLine("3. Inserisci opzione tre ");
+        System.Console.WriteLine("4. Inserisci opzion quattro ");
+        System.Console.WriteLine("5. Insersci opzione cinque ");
+        System.Console.WriteLine("6. Inserisci opzione sei ");
+         //cwl=inserisci autimaticamente il console
 
-        try
+        while (true)
         {
-            string content = File.ReadAllText(filePath);
-            Console.WriteLine("Contenuto del file:");
-            Console.WriteLine(content);
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Si è verificato un errore: {ex.Message}");
+            Console.WriteLine($"Digita...");
+            
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "1":
+                    Console.Beep();
+                    break;
+                case "2":
+                    Console.Beep(750, 300);
+                    break;
+                case "3":
+                    Console.CursorVisible = true;
+                    break;
+                case "4":
+                    Console.CursorVisible = false;
+                    break;
+                case "5":
+                    Console.WriteLine($"Inserisci la frequenza");
+                    int freq = Int32.Parse(Console.ReadLine());
+
+                    Console.WriteLine($"Inserisci durata in ms");
+                    int ms = Int32.Parse(Console.ReadLine());
+
+                    Console.Beep(freq, ms);
+                    break;
+
+
+                default:
+                Console.Clear();
+                    break;
+            }
 
         }
-
-
-
-
     }
 }
