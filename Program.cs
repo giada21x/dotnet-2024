1aa42723e7cc443f2d60843513d7384e366b206b
unknown <giada01.adamo@gmail.com>
2024-01-16 10:19:30 +0100

    tempo di attesa

--- Program.cs
+++ Program.cs
@@ -1,50 +1,26 @@
 ﻿class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-       Console.Clear();
-       bool continua = true;
-       do
+        int timeoutInSeconds = 5; //imposta il tempo di attesa in secondi
+        Task inputTask = Task.Run(() =>
        {
-            Console.Clear();
-
-            Console.WriteLine("Menù di selezione");
-            Console.WriteLine("1.Opzione uno");
-            Console.WriteLine("2.Opzione due");
-            Console.WriteLine("3.Opzione tre");
-            Console.WriteLine("4.Esci");
-            Console.WriteLine("Scegli un opzione");
-            string? scelta = Console.ReadLine();
-
-            switch (scelta)
-            {
-                case "1":
-                    Console.WriteLine("Hai selezionato l'opzione uno");
-                    break;
-                case "2":
-                    Console.WriteLine($"Hai selezionato l'opzione due");
-                    break;
-                case "3":
-                    Console.WriteLine($"Hai selezionato l'opzione tre");
-                    break;
-                case "4":
-                    Console.WriteLine($"Uscita in corso");
-                    return; //con return mi esce dal programma
-                default:
-                    Console.WriteLine($"Selezione non valida. Riprova.");
-                    break;
-
-    
-            }
-            if (continua)
-            {
-                Console.WriteLine($"Premi un tasto per continuare...");
-                Console.ReadKey(); //Aspetta che l'utente prema un tasto prima di mostrare il menu
-                
-                
-            }
+            Console.WriteLine($"Inserisci un input entro {5} secondi");
+            return Console.ReadLine();
             
+        });
+        Task delayTask = Task.Delay(TimeSpan.FromSeconds(timeoutInSeconds));
+
+        if (await Task.WhenAny(inputTask, delayTask) == inputTask)
+        {
+            //l'utente ha inserito l'input
+            string input = await (inputTask as Task<string>);
+            Console.WriteLine($"Hai inserito: {input}");
         }
-        while (continua);
+        else 
+        {
+            //il tempo è scaduto
+            Console.WriteLine("Tempo scaduto!");
+        }
     }
 }
