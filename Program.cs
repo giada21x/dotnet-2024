43037e9ca94542f18d51814b0b594f61ecd0b441
unknown <giada01.adamo@gmail.com>
2024-01-16 09:51:46 +0100

    menu con do-while

--- Program.cs
+++ Program.cs
@@ -2,50 +2,49 @@
 {
     static void Main(string[] args)
     {
-        Console.Clear();
-        System.Console.WriteLine("1. Inserisci opzione uno ");
-        System.Console.WriteLine("2. Insersci opzione due ");
-        System.Console.WriteLine("3. Inserisci opzione tre ");
-        System.Console.WriteLine("4. Inserisci opzion quattro ");
-        System.Console.WriteLine("5. Insersci opzione cinque ");
-        System.Console.WriteLine("6. Inserisci opzione sei ");
-         //cwl=inserisci autimaticamente il console
+       Console.Clear();
+       bool continua = true;
+       do
+       {
+            Console.Clear();
 
-        while (true)
-        {
-            Console.WriteLine($"Digita...");
-            
-            string input = Console.ReadLine();
-            switch (input)
+            Console.WriteLine("Menù di selezione");
+            Console.WriteLine("1.Opzione uno");
+            Console.WriteLine("2.Opzione due");
+            Console.WriteLine("3.Opzione tre");
+            Console.WriteLine("4.Esci");
+            Console.WriteLine("Scegli un opzione");
+            string? scelta = Console.ReadLine();
+
+            switch (scelta)
             {
                 case "1":
-                    Console.Beep();
+                    Console.WriteLine("Hai selezionato l'opzione uno");
                     break;
                 case "2":
-                    Console.Beep(750, 300);
+                    Console.WriteLine($"Hai selezionato l'opzione due");
                     break;
                 case "3":
-                    Console.CursorVisible = true;
+                    Console.WriteLine($"Hai selezionato l'opzione tre");
                     break;
                 case "4":
-                    Console.CursorVisible = false;
-                    break;
-                case "5":
-                    Console.WriteLine($"Inserisci la frequenza");
-                    int freq = Int32.Parse(Console.ReadLine());
-
-                    Console.WriteLine($"Inserisci durata in ms");
-                    int ms = Int32.Parse(Console.ReadLine());
-
-                    Console.Beep(freq, ms);
-                    break;
-
-
+                    Console.WriteLine($"Uscita in corso");
+                    return; //con return mi esce dal programma
                 default:
-                Console.Clear();
+                    Console.WriteLine($"Selezione non valida. Riprova.");
                     break;
-            }
 
+    
+            }
+            if (continua)
+            {
+                Console.WriteLine($"Premi un tasto per continuare...");
+                Console.ReadKey(); //Aspetta che l'utente prema un tasto prima di mostrare il menu
+                
+                
+            }
+            
         }
+        while (continua);
     }
 }
