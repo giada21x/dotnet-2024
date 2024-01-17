1a2a760a9076d66bb303c505e206d8bf17b67fa0
unknown <giada01.adamo@gmail.com>
2024-01-17 14:13:56 +0100

    fizz buzz con for + lista

--- Program.cs
+++ Program.cs
@@ -1,39 +1,79 @@
-﻿class Program
+﻿using System.ComponentModel;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+
+class Program
 {
     static void Main(string[] args)
     {
         
+        List<int> listaFizzBuzz = new List<int>();
+        List<int> listaFizz = new List<int>();
+        List<int> listaBuzz = new List<int>();
         
         for (int i = 0; i < 10; i++)
         {
+            
             if((i % 5 == 0) & (i % 3 == 0) )
             {
-                Console.ForegroundColor = ConsoleColor.Green;
+                listaFizzBuzz.Add(i);
                 Console.WriteLine($"{i} = Fizz-Buzz");
+                
+                
+                
                 
             }
             else if(i % 5 == 0)
             {
-                Console.ForegroundColor = ConsoleColor.Red;
+                listaFizz.Add(i);
                 Console.WriteLine($"{i} = Fizz");
                 
+                
+                
+                
+
             }
             else if (i % 3 == 0)
             {
-                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                listaBuzz.Add(i);
                 Console.WriteLine($"{i} = Buzz");
+                
                 
             }
             else
             {
-                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                
                 Console.WriteLine($"{i} non divisibile");
+                
             }
+            Console.WriteLine($"");
+            
+        
+            
+        
             Thread.Sleep(300);
+            
+            
+            
         }  
-       
-
-       
+        Console.WriteLine("Questa è la lista FizzBuzz");
+        foreach (int nuovo in listaFizzBuzz)
+        {
+            Console.WriteLine($"{nuovo}");
+            
+        }
+        Console.WriteLine("Questa è la lista Fizz");
+        foreach (int nuovo in listaFizz)
+        {
+            Console.WriteLine($"{nuovo}");
+            
+        }
+        Console.WriteLine("Questa è la lista Buzz");
+       foreach (int nuovo in listaBuzz)
+        {
+            Console.WriteLine($"{nuovo}");
+            
+        }
         
             
             
