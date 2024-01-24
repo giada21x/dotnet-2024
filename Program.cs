475f50732f21991404c6f90e9aa7cfd67b471231
unknown <giada01.adamo@gmail.com>
2024-01-24 11:57:25 +0100

    try-catch implementato

--- Program.cs
+++ Program.cs
@@ -1,20 +1,171 @@
-﻿//vogliamo gestire il System.NullReferenceException (si verifica quando si tenta di accedere ad un oggetto null)
+﻿
+
+
+
+using System.ComponentModel;
+
 class Program
 {
    static void Main(string[] args)
    {
-      string? nome = null; //stiamo spingendo volontariamente verso l'errore 
+      Console.WriteLine("INIZIO GIOCO!");
+      Console.WriteLine($"Il punteggio massimo è 20 punti: perdi tot punti in base al suggerimento che sfrutti"); 
+      //max punti = 20 punti
+
+
+
+
+      Random random = new Random(); //generatore di numeri casuali 
+
+      int numeroRandom = random.Next(1, 51);
+      int tentativi = 0;
+      int punteggio = 20;
+      int numero = 0;
+
+      //implementazione del try-catch: verificare se l'elemento inserito è un numero
+      Console.WriteLine("Inserisci un numero:");
+      {
       try
       {
-         Console.WriteLine(nome.Length);
+         
+         numero = int.Parse(Console.ReadLine()!);
+         Console.WriteLine("Il numero è valido");
+            
+            
       }
       catch (Exception e)
       {
-         Console.WriteLine("Il nome non è valido");
-         return;
+         Console.WriteLine("Non hai inserito un numero");
+         Console.WriteLine("Inserisci un numero");
+         numero = int.Parse(Console.ReadLine()!);
+         
          
       }
-   
+      }
+
+     
+
+      while (true)
+      {
+         tentativi++;
+         
+
+
+         if (numero == numeroRandom)
+         {
+
+            Console.WriteLine("HAI VINTO!");
+            Console.WriteLine($"Il numero casuale è {numeroRandom}");
+            Console.WriteLine($"Hai indovinato in {tentativi} tentativi");
+            Console.WriteLine($"Il tuo punteggio è {punteggio}");
+
+            return;
+         }
+
+
+         else
+         {
+
+
+            Console.WriteLine($"Non hai indovinato!");
+            Console.WriteLine($"Vuoi usufruire di un suggerimento? (s/n)");
+
+            string risposta = Console.ReadLine()!;
+            if (risposta == "s")
+
+            {
+
+               Console.WriteLine("Ecco i suggerimenti: quale preferisci?");
+               Console.WriteLine("1. E' maggiore o minore?");
+               Console.WriteLine("2. E' Pari o dispari?");
+               Console.WriteLine("3. E' maggiore o minore di 25?");
+               Console.WriteLine("4. Qual è il suo quadrato? ");
+               ConsoleKeyInfo key = Console.ReadKey(true); //serve per nascondere il numero dell'opzione di scelta che andiamo a inserie manualmente nel terminale
+               string scelta = key.KeyChar.ToString();
+
+
+
+
+               switch (scelta)
+               {
+                  case "1":  //penalità suggerimento 1: -1 punti
+                     if (numeroRandom < numero)
+                     {
+                        punteggio -= 1;
+                        Console.WriteLine("Suggerimento: prova a inserire un numero più piccolo...");
+                        Console.WriteLine($"Sei a {punteggio} punti");
+
+                     }
+                     else
+                     {
+                        punteggio -= 1;
+                        Console.WriteLine("Suggerimento: prova a inserire un numero più grande...");
+                        Console.WriteLine($"Sei a {punteggio} punti");
+                     }
+                     
+
+
+                     break;
+                  case "2": //penalità suggerimento 2: -3 punti
+                     if (numeroRandom % 2 == 0)
+                     {
+                        punteggio -= 3;
+                        Console.WriteLine("Suggerimento: Il numero da indovinare è pari");
+                        Console.WriteLine($"Sei a {punteggio} punti");
+                     }
+                     else
+                     {
+                        punteggio -= 3;
+                        Console.WriteLine("Suggerimento: Il numero da indovinare è pari");
+                        Console.WriteLine($"Sei a {punteggio} punti");
+                     }
+                     break;
+                  case "3": //penalità suggerimento 1: -4 punti
+                     if (numeroRandom > 25)
+                     {
+                        punteggio -= 4;
+                        Console.WriteLine("Suggerimento: Il numero è maggiore di 25 ");
+                        Console.WriteLine($"Sei a {punteggio} punti");
+                     }
+                     else
+                     {
+                        punteggio -= 4;
+                        Console.WriteLine("Suggerimento: Il numero è minore di 25 ");
+                        Console.WriteLine($"Sei a {punteggio} punti");
+                     }
+                     break;
+                  case "4": //penalità suggerimento 1: -5 punti
+                     punteggio -= 5;
+                     double risultato = Math.Pow(numeroRandom, 2);
+                     Console.WriteLine($"Il quadrato è {risultato}");
+                     Console.WriteLine($"Sei a {punteggio} punti");
+
+                     break;
+
+
+               }
+            }
+            else
+            {
+               Console.WriteLine($"Sei a {punteggio} punti");
+
+            }
+
+
+
+            Console.WriteLine("Inserisci un nuovo numero");
+            numero = int.Parse(Console.ReadLine()!);
+
+         }
+
+
+         if (tentativi == 10 || punteggio == 0)
+         {
+            Console.WriteLine($" FINE! HAI PERSO!");
+            Console.WriteLine($"Hai esaurito i tentativi");
+            Console.WriteLine($"Il numero casuale era {numeroRandom}");
+         }
+      }
    }
 }
 
