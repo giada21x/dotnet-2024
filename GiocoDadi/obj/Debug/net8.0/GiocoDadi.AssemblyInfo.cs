d677434babe088c327893071ebeda9dba9f23c15
giada ddjn <giada01.adamo@gmail.com>
2024-02-11 22:27:21 +0100

    gioco dadi

--- GiocoDadi/obj/Debug/net8.0/GiocoDadi.AssemblyInfo.cs
+++ GiocoDadi/obj/Debug/net8.0/GiocoDadi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GiocoDadi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4dfd706b86ea6e9754f3e044a2813292626af43")]
 [assembly: System.Reflection.AssemblyProductAttribute("GiocoDadi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GiocoDadi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
