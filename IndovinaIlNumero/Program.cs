a78594d04109f1a8a3c0c003ca79ae80c74b2d19
unknown <giada01.adamo@gmail.com>
2024-02-12 16:06:15 +0100

    indovina il numero

--- IndovinaIlNumero/Program.cs
+++ IndovinaIlNumero/Program.cs
@@ -5,14 +5,41 @@ class Program
 {
    static void Main()
    {
+      string path = @"test.txt"; // il file deve essere nella stessa cartella del programma
+      string[] lines = File.ReadAllLines(path); //legge tutte le righe del file
+      string[] nomi = new string[lines.Length];
+      
+
+      for (int i = 0; i < lines.Length; i++)
+      {
+         nomi[i] = lines[i];
+      }
+
+      string nomeGiocatore = lines[1];
+      string punteggione = lines[3];
+      Console.Write("Nella partita predente ha giocato ");
+      Console.WriteLine(nomeGiocatore);
+      Console.WriteLine("");
+      Console.WriteLine("Il punteggio della partita precedente: ");
+      Console.WriteLine(punteggione);
+      Console.WriteLine("");
       bool inizio = false;
       while (!inizio)
       {
          Random random = new Random();
          int numeroDaIndovinare = random.Next(1, 51); // Genera un numero casuale tra 1 e 50
          int punteggio = 20;
-         Console.Clear();
+         Console.WriteLine("Inserisci il tuo nome:");
+         
+         string nome = Console.ReadLine()!;
+
+         Console.WriteLine($"Benvenuta/o {nome}");
+         
+         
+         
          Console.WriteLine("Indovina il numero segreto compreso tra 1 e 50. Hai 10 tentativi.");
+         Console.WriteLine("Perdi un punto ad ogni tentativo sbagliato e tot punti per ogni suggerimento che utilizi");
+         
 
          int tentativiRimasti = 10;
          bool indovinato = false;
@@ -51,11 +78,11 @@ class Program
                         {
                            Console.WriteLine("OK! Quale suggerimento vuoi utilizzare?");
 
-                           Console.WriteLine("1. Il numero da indovinare è maggiore o minore?");
-                           Console.WriteLine("2. Il numero da indovinare è pari o dispari?");
-                           Console.WriteLine("3. Il numero da indovinare è minore o maggiore di 25?");
-                           Console.WriteLine("4. Qual è il quadrato del numero da indovinare?");
-                           Console.WriteLine("5. Qual è la somma delle cifre del numero da indovinare?");
+                           Console.WriteLine("1. Il numero da indovinare è maggiore o minore? -1 punto");
+                           Console.WriteLine("2. Il numero da indovinare è pari o dispari? -2 punti");
+                           Console.WriteLine("3. Il numero da indovinare è minore o maggiore di 25? -3 punti");
+                           Console.WriteLine("4. Qual è il quadrato del numero da indovinare? -5 punti");
+                           Console.WriteLine("5. Qual è la somma delle cifre del numero da indovinare? -4 punti");
                            string suggerimento = Console.ReadLine()!;
                            switch (suggerimento)
                            {
@@ -101,11 +128,13 @@ class Program
                   }
                   else
                   {
-                     Console.WriteLine($"Bravo! Hai indovinato il numero segreto. Il tuo punteggio finale è: {punteggio}.");
+                     Console.WriteLine($"Bravo! Hai indovinato il numero segreto. Il tuo punteggio finale è: {punteggio}/20.");
+                  lines[1] = nome;
+                  lines[3] = punteggio.ToString();
+                  File.WriteAllLines(path, lines);
+                  
+                  Console.WriteLine("Salvataggio...");
                      return;
-
-
-
                   }
 
                }
